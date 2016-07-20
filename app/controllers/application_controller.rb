5aa21bf65f5f0be238128f82b034717f984842fc
Dong Cong Phuong <gongfangdong@gmail.com>
2016-07-20 22:02:10 +0700

    a little change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render html: "Hello world!"
+  	render html: "Hello world! Ahihi"
   end
 end
